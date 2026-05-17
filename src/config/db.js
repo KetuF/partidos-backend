6eab4dc9f6c9fac3f4c7dd052a6c4cc51741caf2
Emanuel Jesus Figueredo <emanuel.j.figueredo@gmail.com>
2026-05-16 23:32:09 -0300

    fix dev env

--- src/config/db.js
+++ src/config/db.js
@@ -2,21 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
-
-(async () => {
-    try {
-        const connection = await db.getConnection();
-        console.log('Conectado a MySQL');
-        connection.release();
-    } catch (error) {
-        console.error('Error MySQL:', error);
-    }
-})();
 
 module.exports = db;
