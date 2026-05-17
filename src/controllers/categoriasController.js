9813be3d6743fec37dc40fe0667dc380d0a75e95
Emanuel Jesus Figueredo <emanuel.j.figueredo@gmail.com>
2026-05-16 23:44:20 -0300

    debug categorias

--- src/controllers/categoriasController.js
+++ src/controllers/categoriasController.js
@@ -2,11 +2,22 @@ const db = require('../config/db');
 
 const GetCategorias = async (req, res) => {
     try {
+
+        console.log('ENTRO A CATEGORIAS');
+
         const [rows] = await db.query('SELECT * FROM categorias');
+
+        console.log(rows);
+
         res.json(rows);
+
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ mensaje: 'Error al obtener categorias' });
+
+        console.log('ERROR SQL:', error);
+
+        res.status(500).json({
+            mensaje: error.message
+        });
     }
 };
 
