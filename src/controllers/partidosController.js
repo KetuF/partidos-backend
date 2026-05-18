4e53e9297b4e76abe406e687f75cd6efeea4c11b
Emanuel Jesus Figueredo <emanuel.j.figueredo@gmail.com>
2026-05-18 00:19:52 -0300

    add numero_fecha

--- src/controllers/partidosController.js
+++ src/controllers/partidosController.js
@@ -4,7 +4,7 @@ const GetPartidosPorCategoria = async (req, res) => {
     try {
         const { categoria_id } = req.params;
         const [rows] = await db.query(`
-            SELECT p.id, p.fecha, p.hora, p.jugado,
+            SELECT p.id, p.fecha, p.hora, p.jugado, p.numero_fecha,
                    p.goles_local, p.goles_visitante,
                    el.nombre as equipo_local,
                    ev.nombre as equipo_visitante
@@ -12,7 +12,7 @@ const GetPartidosPorCategoria = async (req, res) => {
             JOIN equipos el ON p.equipo_local_id = el.id
             JOIN equipos ev ON p.equipo_visitante_id = ev.id
             WHERE p.categoria_id = ?
-            ORDER BY p.fecha ASC, p.hora ASC
+            ORDER BY p.numero_fecha ASC, p.hora ASC
         `, [categoria_id]);
         res.json(rows);
     } catch (error) {
